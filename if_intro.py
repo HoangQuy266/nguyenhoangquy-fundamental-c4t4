2bea0d5907c2652c094437160c1e7d62b4b7a7a2
HoangQuy266 <40481392+HoangQuy266@users.noreply.github.com>
2018-06-22 13:09:42 +0700

    Delete if_intro.py

--- if_intro.py
+++
@@ -1,9 +0,0 @@
-yob=int(input("Your year of birth")) #input
-age = 2018 -yob #process
-if age <10:
-    print("Baby")
-elif age < 18 :
-    print("teenager")
-else:
-    print("Not baby")
-# print("Your age:",age) output
